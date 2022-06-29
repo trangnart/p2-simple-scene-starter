be2f06dbb61c28a65b355110216397c44918a99f
Jane <tranjtrang@gmail.com>
2022-06-28 18:56:19 -0700

    final push

---
+++ README.md
@@ -0,0 +1,17 @@
+# Moody Sky
+Description: A calm scenario that control by the moody sun.
+
+Reference inspo: [image]
+
+Explain how your scene uses randomness: i.e. what is the pattern you observe in the inspiring image? What is the pattern you are using to place your shapes (it could be a simplification)?
+
+- *Clouds are the main object that fill half of the inspo image so I want the audience to focus in the cloud by using randomness. Clouds are created using ellipse shape, rectangle for terrain, generate random end point shape for hills, circle shape for the sun.*
+
+Explain how your scene uses reactivity.  What is the input that it reacts to and what is the logic and reasoning for the reaction?
+
+- *The sun follows the user mouse and the background color change based on the mouse's movement. I choose this interaction because the audience can control the scenario based on their mood. The scenario will react to the sun's movement.*
+
+Artist statement: What is the effect you're trying to achieve? Did you achieve that effect? If not, what was the difficulty/limitation with tools, and what did you end up doing?
+- At first, I want clouds follow the user mouse but I didn't achieve it because the random setup make it difficult to randomize the clouds position and set clouds follow the mouse at the same time. I end up using the sun instead because it doesn't bind with randomness.
+
+[image]: https://www.123rf.com/photo_37413838_dramatic-cloud-and-the-vast-grassland-of-savanna.html
--- script.js
+++ script.js
@@ -1,40 +1,40 @@
-let seed = 12345;
-let cloudx = 100;
-let cloudy = 100;
-let blue = 189;
+let seed = 1234;
 const cloudColor = "#f4fafc";
 const grassColor ="#d9b44a";
-const hillColor = "#1e273f";
-var drop = []
+const hillColor = "#152238";
+var color;
 
 function setup() {
   createCanvas(700, 500);
-  for(var i = 0; i < 200; i++) {
-    drop[i] = new Drop();
-  }
   createButton("reroll").mousePressed(() => seed++);
 }
 
 function draw() {
   randomSeed(seed);
-  background(100, 100, mouseY);
 
-  fill(cloudColor);
-  ellipse(50, 20, 300, 100);
-  ellipse(300, 25, 300, 150);
-  ellipse(500, 20, 300, 100);
-  ellipse(680, 20, 150, 100);
+  color=map(mouseY,50,360,200,0);
+  background (color);
 
-  fill(grassColor);
-  rect(0, 420, width, 80);
+  fill(224,176,186);
+  noStroke();
+  circle (mouseX,mouseY,100);
 
+  fill(hillColor);
+  beginShape();
+  vertex(0, height + 50);
   const steps = 10;
   for (let i = 0; i < steps + 1; i++) {
     let x = (width * i) / steps;
     let y =
-      height - (random() * random() * random() * height) / 8 - height / 50;
+      height - 80 - (random() * random() * random() * height) / 8 - height / 50;
     vertex(x, y);
   }
+  vertex(width, height + 50);
+  endShape(CLOSE);
+
+  fill(grassColor);
+  rect(0, 410, width, 100);
+
   const clouds = 5 * random();
   for (let i = 0; i < clouds; i++) {
     setCloud();
@@ -43,10 +43,10 @@ function draw() {
   function setCloud() {
     let x = width * random();
     let y = height/2 * random();
-    makeCloud(x, y);
+    drawCloud(x, y);
   }
 
-  function makeCloud(cloudx, cloudy) {
+  function drawCloud(cloudx, cloudy) {
     fill(cloudColor);
     noStroke();
 
@@ -57,30 +57,10 @@ function draw() {
     ellipse(cloudx+100, cloudy+20, 50, 50);
     ellipse(cloudx+170, cloudy+20, 100, 60);
     ellipse(cloudx+175, cloudy-15, 50, 50);
-
   }
-  for(var i = 0; i < 200; i++) {
-    drop[i].show();
-    drop[i].update();
-  }
-}
-function Drop() {
-  this.x = random(0, width);
-  this.y = random(0, -height);
-
-  this.show = function() {
-    noStroke();
-    fill(255);
-    ellipse(this.x, this.y, random(1, 5), random(1, 5));
-  }
-  this.update = function() {
-    this.speed = random(5, 10);
-    this.gravity = 1;
-    this.y = this.y + this.speed*this.gravity;
-
-    if (this.y > height) {
-      this.y = random(0, -height);
-      this.gravity = 0;
-    }
-  }
+  fill(cloudColor);
+  ellipse(50, 20, 300, 100);
+  ellipse(300, 25, 300, 150);
+  ellipse(500, 20, 300, 100);
+  ellipse(680, 20, 150, 100);
 }
